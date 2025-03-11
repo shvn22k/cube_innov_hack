653d256a98f7c4957c7936dc5ddd6c3f421a8587
Mayank Gupta <mayankkumargupta03@gmail.com>
2025-03-11 21:53:35 +0530

    db connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
-
+import connect from "@config/db";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+connect();
 app.listen(8000,()=>{
     console.log("Server is running on port 8080");
 })
